6ad3d1f2e9bfce5e82cdff72da7037764926378f
Yurii Shulhin <helios@Yuriis-MacBook-Air.local>
2026-01-30 13:01:13 +0200

    Solution

---
+++ app/knights/models.py
@@ -0,0 +1,31 @@
+class Knight:
+    def __init__(self, config: dict) -> None:
+        self.name = config["name"]
+        self.power = config["power"]
+        self.hp = config["hp"]
+        self.protection = 0
+
+        # Викликаємо методи для підрахунку статів одразу при створенні
+        self._apply_armour(config["armour"])
+        self._apply_weapon(config["weapon"])
+        self._apply_potion(config["potion"])
+
+    def _apply_armour(self, armour_list: list) -> None:
+        for item in armour_list:
+            self.protection += item["protection"]
+
+    def _apply_weapon(self, weapon: dict) -> None:
+        self.power += weapon["power"]
+
+    def _apply_potion(self, potion: dict) -> None:
+        if potion:
+            effect = potion["effect"]
+            self.hp += effect.get("hp", 0)
+            self.power += effect.get("power", 0)
+            self.protection += effect.get("protection", 0)
+
+    def take_damage(self, damage: int) -> None:
+        """Зменшує HP лицаря, враховуючи, що HP не може бути < 0."""
+        self.hp -= damage
+        if self.hp < 0:
+            self.hp = 0
